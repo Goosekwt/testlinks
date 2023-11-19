f66edd143bbc309fa7059c78e9fb0467566c8d63
Goosekwt <t.alsaleh98@gmail.com>
2023-11-20 00:40:23 +0300

    test
    test

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
-  if (window.DeviceOrientationEvent) {
-    window.addEventListener("deviceorientation", handleOrientation, true);
-  } else {
-    console.error("Device orientation not supported.");
+  var animationElement = document.querySelector(".portrait-animation");
+
+  // Set the initial playback rate based on scroll position
+  updatePlaybackRate();
+
+  // Update playback rate when the user scrolls
+  window.addEventListener("scroll", function() {
+    updatePlaybackRate();
+  });
+
+  function updatePlaybackRate() {
+    // Get the scroll position
+    var scrollPosition = window.scrollY;
+
+    // Map the scroll position to the playback rate (adjust as needed)
+    var playbackRate = mapRange(scrollPosition, 0, document.body.clientHeight, 1, 2);
+
+    // Set the playback rate for the animation
+    animationElement.playbackRate = playbackRate;
   }
 
-  function handleOrientation(event) {
-    var rotation = event.gamma;
-    var adjustedRotation = rotation * 2;
-    document.querySelector(".portrait-animation").style.transform = "rotate(" + adjustedRotation + "deg)";
+  // Helper function to map a value from one range to another
+  function mapRange(value, fromMin, fromMax, toMin, toMax) {
+    return (value - fromMin) * (toMax - toMin) / (fromMax - fromMin) + toMin;
   }
 });

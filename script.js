454865440db68e147c99d9a35477c434abce8063
Goosekwt <t.alsaleh98@gmail.com>
2023-11-20 01:41:42 +0300

    g
    g

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
     var video = document.getElementById('scrollVideo');
+    var lastScrollTop = 0;
+    var videoDuration = 33; // Duration of your video in seconds
     var setHeight = document.documentElement.scrollHeight - window.innerHeight;
-    var playbackConst = setHeight / 33; // Adjust this value as needed
+    var maxScroll = setHeight - 10; // 10 is a small offset
 
-    // Ensure video metadata is loaded
-    video.addEventListener('loadedmetadata', function() {
-        // Set up scroll event
-        window.addEventListener('scroll', function() {
-            var scrollPosition = window.pageYOffset;
-            var videoTime = scrollPosition / playbackConst;
-            video.currentTime = videoTime;
-        });
-    });
+    window.addEventListener('scroll', function() {
+        var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        var scrollFraction = scrollTop / maxScroll;
+        var frameIndex = Math.min(videoDuration * scrollFraction, videoDuration);
+
+        if (scrollTop > lastScrollTop) {
+            // Scrolling Down
+            video.currentTime = frameIndex;
+        } else {
+            // Scrolling Up
+            video.currentTime = frameIndex;
+        }
+
+        lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
+    }, false);
 });

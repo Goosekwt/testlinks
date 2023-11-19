7e1c49f660df4722fc61939fae922004e3231b3f
Goosekwt <t.alsaleh98@gmail.com>
2023-11-20 00:58:52 +0300

    d
    d

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
   var animationElement = document.querySelector(".portrait-animation");
-  var isPlaying = false;
 
   // Update playback rate when the user scrolls
   window.addEventListener("scroll", function() {
-    if (!isPlaying) {
-      playVideo();
-      isPlaying = true;
-    }
+    playVideo();
   });
 
   function playVideo() {

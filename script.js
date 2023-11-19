9a1e4df098e5ec307326f91c2503fea890126a4c
Goosekwt <t.alsaleh98@gmail.com>
2023-11-20 01:50:36 +0300

    d
    d

--- script.js
+++ script.js
@@ -1,39 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
     var video = document.getElementById('scrollVideo');
-    var lastPosition = -1;
     var videoDuration = 33; // Duration of your video in seconds
-    var setHeight = document.documentElement.scrollHeight - window.innerHeight;
-    var maxScroll = setHeight - 10;
+    var lastScrollTop = 0; // Last scroll position
 
-    function updateVideoOnScroll(scrollTop) {
-        var scrollFraction = scrollTop / maxScroll;
-        var frameIndex = Math.min(videoDuration * scrollFraction, videoDuration);
-        video.currentTime = frameIndex;
-        console.log('ScrollTop:', scrollTop, 'Video Time:', video.currentTime);
+    function adjustVideoPlayback() {
+        var scrollPercent = window.pageYOffset / (document.body.offsetHeight - window.innerHeight);
+        var time = scrollPercent * videoDuration;
+        video.currentTime = Math.min(time, video.duration);
     }
 
     window.addEventListener('scroll', function() {
-        var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-        if (scrollTop != lastPosition) {
-            updateVideoOnScroll(scrollTop);
-            lastPosition = scrollTop;
-        }
-    }, false);
+        adjustVideoPlayback();
+    });
 
-    var startTouchY;
-    var touchSensitivity = 5;
+    // Touch event listeners for mobile devices
+    var touchStart = 0;
 
     window.addEventListener('touchstart', function(e) {
-        startTouchY = e.touches[0].clientY;
+        touchStart = e.touches[0].pageY;
     }, false);
 
     window.addEventListener('touchmove', function(e) {
-        var touchY = e.touches[0].clientY;
-        var touchMove = startTouchY - touchY;
-        var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-        if (Math.abs(touchMove) > touchSensitivity) {
-            updateVideoOnScroll(scrollTop + touchMove);
-        }
+        var touchEnd = e.touches[0].pageY;
+        var touchDistance = touchStart - touchEnd;
+        window.scrollBy(0, touchDistance);
+        adjustVideoPlayback();
     }, false);
 });

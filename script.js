12e8b3c20d8e9f8f6b416c5e676d5224264993da
Goosekwt <t.alsaleh98@gmail.com>
2023-11-20 01:22:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
   var contentContainer = document.getElementById("content-container");
   var videoElement = document.getElementById("portraitAnimation");
-  var increment = 0.2; // Adjust the increment for smoother scrolling
+  var increment = 0.1; // Adjust the increment for smoother scrolling
+  var isScrolling = false;
+  var scrollStartTime = 0;
+  var maxScrollDuration = 2000; // Adjust the duration for the deceleration effect
 
   // Initial video element
   createVideoElement();
 
   // Handle wheel events for mouse scrolling
   contentContainer.addEventListener("wheel", function(event) {
+    event.preventDefault(); // Prevent default scrolling behavior
+
     // Check the direction of the wheel scroll
     var direction = (event.deltaY > 0) ? 1 : -1;
 
-    // Adjust the playback time based on the wheel direction
-    adjustPlayback(direction * increment);
+    // Start scrolling animation
+    startScrollAnimation(direction * increment);
   });
 
   // Handle touch events for mobile swiping
@@ -26,8 +31,8 @@ document.addEventListener("DOMContentLoaded", function() {
     var touchEndY = event.changedTouches[0].clientY;
     var swipeDirection = (touchEndY > touchStartY) ? -1 : 1;
 
-    // Adjust the playback time based on the swipe direction
-    adjustPlayback(swipeDirection * increment);
+    // Start scrolling animation
+    startScrollAnimation(swipeDirection * increment);
   });
 
   function createVideoElement() {
@@ -43,6 +48,29 @@ document.addEventListener("DOMContentLoaded", function() {
     contentContainer.appendChild(newVideoElement);
   }
 
+  function startScrollAnimation(scrollIncrement) {
+    if (!isScrolling) {
+      isScrolling = true;
+      scrollStartTime = performance.now();
+
+      // Initiate the animation loop
+      requestAnimationFrame(function animateScroll(timestamp) {
+        var elapsed = timestamp - scrollStartTime;
+        var progress = Math.min(elapsed / maxScrollDuration, 1);
+
+        // Adjust the playback time based on the easing function
+        adjustPlayback(easeOutQuad(progress) * scrollIncrement);
+
+        // Continue the animation until it reaches the end
+        if (progress < 1) {
+          requestAnimationFrame(animateScroll);
+        } else {
+          isScrolling = false;
+        }
+      });
+    }
+  }
+
   function adjustPlayback(seconds) {
     // Adjust the playback time of the current video element
     videoElement.currentTime += seconds;
@@ -53,5 +81,10 @@ document.addEventListener("DOMContentLoaded", function() {
     } else if (videoElement.currentTime > videoElement.duration) {
       videoElement.currentTime = videoElement.duration;
     }
+  }
+
+  // Easing function for smoother animation
+  function easeOutQuad(t) {
+    return t * (2 - t);
   }
 });

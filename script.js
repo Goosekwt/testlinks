c6f216835f446ed396f05c165b74e3be45a444d1
Goosekwt <t.alsaleh98@gmail.com>
2023-11-20 00:54:47 +0300

    t
    t

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
   var animationElement = document.querySelector(".portrait-animation");
-  var lastScrollPosition = window.scrollY;
+  var isPlaying = false;
 
   // Update playback rate when the user scrolls
   window.addEventListener("scroll", function() {
-    var currentScrollPosition = window.scrollY;
-    var scrollDirection = (currentScrollPosition > lastScrollPosition) ? "down" : "up";
-
-    // Set the playback rate based on scroll direction
-    var playbackRate = (scrollDirection === "down") ? 1 : -1;
-
-    // Set the playback rate for the animation
-    animationElement.playbackRate = playbackRate;
-
-    // Update the last scroll position
-    lastScrollPosition = currentScrollPosition;
+    if (!isPlaying) {
+      playVideo();
+      isPlaying = true;
+    }
   });
+
+  function playVideo() {
+    // Play the video
+    animationElement.play();
+  }
 });

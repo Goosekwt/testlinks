8182a9afa1b452de53e74620838d320be3045d98
Goosekwt <t.alsaleh98@gmail.com>
2023-11-20 01:55:31 +0300

    d
    dd

--- script.js
+++ script.js
@@ -1,29 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     var video = document.getElementById('scrollVideo');
-    var videoDuration = 33; // Duration of your video in seconds
-    var lastScrollTop = 0; // Last scroll position
-
-    function adjustVideoPlayback() {
-        var scrollPercent = window.pageYOffset / (document.body.offsetHeight - window.innerHeight);
-        var time = scrollPercent * videoDuration;
-        video.currentTime = Math.min(time, video.duration);
-    }
+    var lastScrollTop = 0;
+    var videoDuration = 33; // Video duration in seconds
 
     window.addEventListener('scroll', function() {
-        adjustVideoPlayback();
+        var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        var delta = scrollTop - lastScrollTop;
+        lastScrollTop = scrollTop;
+
+        // Adjust playback speed and direction
+        var timeToSeek = video.currentTime + (delta / 1000); // Smaller number = faster playback
+        if (timeToSeek >= 0 && timeToSeek <= videoDuration) {
+            video.currentTime = timeToSeek;
+        }
     });
-
-    // Touch event listeners for mobile devices
-    var touchStart = 0;
-
-    window.addEventListener('touchstart', function(e) {
-        touchStart = e.touches[0].pageY;
-    }, false);
-
-    window.addEventListener('touchmove', function(e) {
-        var touchEnd = e.touches[0].pageY;
-        var touchDistance = touchStart - touchEnd;
-        window.scrollBy(0, touchDistance);
-        adjustVideoPlayback();
-    }, false);
 });
